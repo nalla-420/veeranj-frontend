89413d629082d17ffb9aad737a0254aaf556b821
nalla-420 <thekamine159@gmail.com>
2026-05-13 13:11:17 +0530

    Initialize main.jsx with React application setup

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import RestaurantApp from './RestaurantApp'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RestaurantApp />
+  </React.StrictMode>
+)
